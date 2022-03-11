7bdb1f6af49103563cb145972c7d7ecef87e3385
Juan Pedro Tovar <jptovar92@gmail.com>
2022-03-10 21:53:45 -0300

    Elegir el sombrero y limpieza

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 });
 
 // Se devuelve un JSON con un arreglo de nombres alojado en el servidor.
-app.get("/usuarios", (req, res) => {
+app.get("/abracadabra/usuarios", (req, res) => {
 	// Lectura del archivo usuarios.json
 	fs.readFile("assets/nombres.json", "utf8", (err, data) => {
 		if (err) {
@@ -28,21 +28,43 @@ app.get("/usuarios", (req, res) => {
 });
 
 // A traves de un middleware se valida la existencia del nombre en el el archivo JSON
-app.use("/juego/:usuario", (req, res, next) => {
+app.use("/abracadabra/juego/:usuario", (req, res, next) => {
 	const nombres = JSON.parse(fs.readFileSync("assets/nombres.json", "utf8"));
 
 	for (const usuario of nombres.usuarios) {
 		if (usuario === req.params.usuario) {
-			res.send(`El usuario ${usuario} existe en el juego`);
+			res.send(`<div style="text-align: center">
+						<h1 style="color: green">El usuario ${usuario} existe en el juego</h1>	
+					</div>`);
 			return;
 		}
 	}
 	next();
 });
 
-app.get("/juego/:usuario", (req, res) => {
+app.get("/abracadabra/juego/:usuario", (req, res) => {
 	const usuario = req.params.usuario;
-	res.send(`El usuario ${usuario} no existe en el juego`);
+	res.setHeader("Content-Type", "text/html");
+	res.send(`<div style="text-align: center">
+				<h1>El usuario ${usuario} no existe en el juego</h1>
+				<img src="/who.jpeg" alt="No existe">	
+			</div>`);
+});
+
+// Basado en un número aleatorio del 1 al 4, devolver la foto
+// del conejo en caso de coincidir con el número recibido como parámetro ó devolver la
+// foto de Voldemort en caso de no coincidir.
+
+app.get("/abracadabra/conejo/:n", (req, res) => {
+	const n = req.params.n;
+	const numero = Math.floor(Math.random() * 4) + 1;
+
+	console.log(`El número recibido es ${n}`);
+	console.log(`El número aleatorio es ${numero}`);
+
+	numero == n
+		? res.sendFile(__dirname + "/assets/conejito.jpg")
+		: res.sendFile(__dirname + "/assets/voldemort.jpg");
 });
 
 // Inicializacion del servidor en el puerto 3000
